13a3db986c9e2a53436774bed08d4661268ca9cb
Sclipse <karlo.versic@gmail.com>
2023-11-29 08:53:02 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "RefereeForge",
+  description:
+    "RefereeForge is the ultimate companion app designed to sculpt and refine the skills of aspiring karate referees. Through interactive modules users can forge their judgment abilities, ensuring they are well-prepared for karate referee exams.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
